fb929e9301fd42ae50a7279c10787cb9bf8462cc
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-05 10:44:30 +1100

    testing

--- Clock.js
+++ Clock.js
@@ -1,8 +1,10 @@
-let link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'Clock.css';
-let head = document.head;
-head.appendChild(link);
+const loadCSS = (url) => {
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url;
+    let head = document.head;
+    head.appendChild(link);
+}
 
 const clock = () => {
     let bgColor = '#06134d';
@@ -13,8 +15,9 @@ const clock = () => {
     let hour = date.getHours().toString().padStart(2, '0');
     let minute = date.getMinutes().toString().padStart(2, '0');
     let second = date.getSeconds().toString().padStart(2, '0');
-    console.log(`${year}/${month}/${day} ${hour}:${minute}:${second}`);
-    document.getElementById('date').innerHTML = `${year}/${month}/${day}`;
+    //console.log(`${year}/${month}/${day} ${hour}:${minute}:${second}`);
+    document.querySelector('#date').innerHTML = `<p>${year}/${month}/${day}</p>`;
+    
     if (second%2){
         document.getElementById('time').innerHTML = `${hour}:${minute}:${second} `;
     }
@@ -22,10 +25,56 @@ const clock = () => {
         document.getElementById('time').innerHTML = `${hour}<span style="color:${bgColor}">:</span>${minute}<span style="color:${bgColor}">:</span>${second} `;
     }
 }
-    
-clock();
 
+function addURLParam(url, name, value) {
+    url += (url.indexOf("?") == -1 ? "?" : "&");
+    url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    return url;
+} 
+
+document.body.onload = () => {
+    console.log('loaded')
+}
+document.body.onresize = () => {
+    console.log(`size is ${window.innerWidth}x${window.innerHeight}`)
+}
+document.oncontextmenu = (e) =>{
+    e.preventDefault();
+}
+
+document.onpageshow = (e) =>{
+    console.log('show')
+}
+
+document.onclick = (e) => {
+    if (e.shiftKey){
+        console.log('with shift');
+    }
+    console.log(`Position clicked: (${e.pageX}, ${e.pageY})`)
+}
+
+document.ondblclick = () => {
+    console.log('Don\'t double click!')
+}
+
+document.textInput = (e) => {
+    console.log(e.key)
+}
+
+
+document.body.querySelector('#clock').onmouseenter = () => {
+    console.log('On the clock')
+}
+
+document.body.querySelector('#clock').onmouseleave = () => {
+    console.log('Off the clock')
+}
+
+
+//clock main functions
+loadCSS('Clock.css');
+clock();
 setInterval(() => {
    clock();
+   
 }, 1000); 
-
